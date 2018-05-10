dfcac9755ea4b61bd0ee208a6cb48d68ee7ae5cd
mizchi <miz404@gmail.com>
2018-05-11 03:04:24 +0900

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 /// <reference> declarations.d.ts
 
-import git from "isomorphic-git";
-import BrowserFS from "browserfs";
-console.log(BrowserFS);
-
-BrowserFS.install(window);
-
-BrowserFS.configure({ fs: "IndexedDB", options: {} }, (err: any) => {
-  if (err) return console.log(err);
-  const fs = BrowserFS.BFSRequire("fs");
-  git.listFiles({ fs, dir: "/" });
-});
+console.log("hello!")
