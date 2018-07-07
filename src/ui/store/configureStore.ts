ac007db7786a02fcca36308645e168a13d74e742
mizchi <miz404@gmail.com>
2018-07-07 21:30:47 +0900

    Use version to persisit key

--- src/ui/store/configureStore.ts
+++ src/ui/store/configureStore.ts
@@ -1,19 +1,20 @@
-import { applyMiddleware, createStore } from "redux";
-import logger from "redux-logger";
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import promise from "redux-promise";
-import thunk from "redux-thunk";
-import { rootReducer } from "../reducers";
+import { applyMiddleware, createStore } from "redux"
+import logger from "redux-logger"
+import { persistReducer, persistStore } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import promise from "redux-promise"
+import thunk from "redux-thunk"
+import pkg from "../../../package.json"
+import { rootReducer } from "../reducers"
 
 const persistConfig = {
-  key: 'root',
-  storage,
+  key: `root@${pkg.version}`,
+  storage
 }
 
 export function configureStore() {
   const persistedReducer = persistReducer(persistConfig, rootReducer as any)
-  const store =  createStore(
+  const store = createStore(
     persistedReducer,
     applyMiddleware(thunk, promise, logger)
   )
