56f8831cb33e73fdbe718f9424c67ce5bded1ec3
mizchi <miz404@gmail.com>
2018-07-07 21:20:59 +0900

    Add redux-persisit

--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -1,10 +1,16 @@
 import React from "react"
 import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
 import { configureStore } from "../store/configureStore"
 import { StackRouter } from "./utils/StackRouter"
 
-export const App = () => (
-  <Provider store={configureStore()}>
-    <StackRouter />
-  </Provider>
-)
+export const App = () => {
+  const { store, persistor } = configureStore()
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <StackRouter />
+      </PersistGate>
+    </Provider>
+  )
+}
--- src/ui/store/configureStore.ts
+++ src/ui/store/configureStore.ts
@@ -1,12 +1,22 @@
-import { applyMiddleware, createStore } from "redux"
-import logger from "redux-logger"
-import promise from "redux-promise"
-import thunk from "redux-thunk"
-import { rootReducer } from "../reducers"
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import promise from "redux-promise";
+import thunk from "redux-thunk";
+import { rootReducer } from "../reducers";
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
 export function configureStore() {
-  return createStore(
-    rootReducer as any,
+  const persistedReducer = persistReducer(persistConfig, rootReducer as any)
+  const store =  createStore(
+    persistedReducer,
     applyMiddleware(thunk, promise, logger)
   )
+  const persistor = persistStore(store)
+  return { store, persistor }
 }
