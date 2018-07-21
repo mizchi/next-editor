f690a688a4c3ec7acba154028edf5870dda23af2
mizchi <mz@chikubakoutarounoMacBook-puro.local>
2018-07-21 20:55:27 +0900

    Remove storybook and update babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const webpack = require("webpack")
+const path = require('path')
 // const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin")
-const path = require("path")
 const CopyPlugin = require("copy-webpack-plugin")
-const mode = process.env.NODE_ENV || "development"
 const WorkboxPlugin = require("workbox-webpack-plugin")
 
 const MODE = process.env.NODE_ENV || "development"
@@ -32,10 +31,21 @@ module.exports = {
         test: /\.js$/,
         include: [
           path.join(__dirname, "src"),
-          path.join(__dirname, "node_modules/react-icons")
         ],
         use: {
           loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.js$/,
+        include: [
+          path.join(__dirname, "node_modules/react-icons")
+        ],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-react']
+          }
         }
       },
       {
