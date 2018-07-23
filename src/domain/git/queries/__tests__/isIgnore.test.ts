9ad30d17ec94396ba26e2f9aa19a8ca55d727a6c
mizchi <miz404@gmail.com>
2018-07-24 05:27:02 +0900

    feat: isIgnore

---
+++ src/domain/git/queries/__tests__/isIgnore.test.ts
@@ -0,0 +1,18 @@
+import fs from "fs"
+import path from "path"
+import assert from "power-assert"
+import * as helpers from "../../__testHelpers__/helpers"
+import { isIgnored } from "../isIgnored"
+
+test("list files", async () => {
+  const root = await helpers.createTempGitProject()
+  await fs.promises.mkdir(path.join(root, "public"))
+  await helpers.batchUpdateFiles(root, [
+    ["a", "1"],
+    [".gitignore", "b\npublic"]
+  ])
+  await fs.promises.writeFile(path.join(root, "b"), "2")
+  await fs.promises.writeFile(path.join(root, "public/out"), "out")
+  assert(await isIgnored(root, "b"))
+  assert(await isIgnored(root, "public/out"))
+})
