652a9de4feb2d8a11e35a2f36659e76a41fca2ad
mizchi <miz404@gmail.com>
2018-07-24 04:28:44 +0900

    refactor: TextEditor

--- src/ui/components/atoms/TextEditor.tsx
+++ src/ui/components/atoms/TextEditor.tsx
@@ -2,13 +2,32 @@ import { darken } from "polished"
 import React from "react"
 import styled from "styled-components"
 
-export const TextEditor: React.ComponentType<{
+type Props = {
   fontScale: number
+  spellCheck: boolean
+  value: string
+  onChange: (value: string) => void
+}
+
+export const TextEditor = (props: Props) => {
+  return (
+    <Textarea
+      spellCheck={props.spellCheck}
+      value={props.value}
+      onChange={(ev: any) => {
+        props.onChange(ev.target.value)
+      }}
+    />
+  )
+}
+
+export const Textarea: React.ComponentType<{
+  fontScale?: number
   spellCheck: boolean
   value: string
   onChange: any
 }> = styled.textarea`
-  font-size: ${p => p.fontScale}em;
+  font-size: ${p => p.fontScale || "1"}em;
   line-height: 1.5em;
   background: ${p => darken(0.05, p.theme.main)};
   color: ${p => p.theme.textColor};
