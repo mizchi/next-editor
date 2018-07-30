9c4ba1aafe348c43d05e827ee751b423e30a498c
mizchi <miz404@gmail.com>
2018-07-30 18:17:54 +0900

    refactor: command map

--- src/ui/components/utils/GlobalKeyHandler.tsx
+++ src/ui/components/utils/GlobalKeyHandler.tsx
@@ -1,13 +1,39 @@
 import React from "react"
-import { connector } from "../../actionCreators"
+import { HotKeys } from "react-hotkeys"
+import { connect } from "react-redux"
+import { defaultKeyMap, ImplCommandList, implCommandMap } from "./commandMap"
 
-export const GlobalKeyHandler = connector(
-  _state => {
-    return {}
-  },
-  _actions => {
-    return {}
-  }
-)(function GlobalKeyHandlerImpl({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+type Props = { children: React.ReactNode; dispatch: any }
+
+export const GlobalKeyHandler = connect(_state => {
+  return {}
+})(function GlobalKeyHandlerImpl({ children, dispatch }: Props) {
+  const keyMap = { ...defaultKeyMap }
+  const handlers: {
+    [key in ImplCommandList]: (dispatch: any, event: any) => void
+  } = Object.keys(ImplCommandList).reduce(
+    (acc, _key: any) => {
+      const key: ImplCommandList = _key
+      const fn = implCommandMap[key]
+      if (fn) {
+        return {
+          ...acc,
+          [key]: (event: Event) => fn(dispatch, event)
+        }
+      }
+      return acc
+    },
+    {} as any
+  )
+
+  return (
+    <HotKeys
+      focused
+      keyMap={keyMap as any}
+      handlers={handlers as any}
+      style={{ width: "100%", height: "100%" }}
+    >
+      {children}
+    </HotKeys>
+  )
 })
