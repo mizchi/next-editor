467a5c65126620c3cca828f029744a9135010445
mizchi <miz404@gmail.com>
2018-09-01 21:48:56 +0900

    performance: use status matrix for ignore

--- src/domain/filesystem/queries/readFileStats.ts
+++ src/domain/filesystem/queries/readFileStats.ts
@@ -1,8 +1,8 @@
 import fs from "fs"
+import * as git from "isomorphic-git"
 import orderBy from "lodash/orderBy"
 import path from "path"
 import pify from "pify"
-import { isIgnored } from "../../git/queries/isIgnored"
 import { FileInfo } from "../../types"
 
 export async function readFileStats(
@@ -11,14 +11,26 @@ export async function readFileStats(
 ): Promise<FileInfo[]> {
   const filenames: string[] = await pify(fs.readdir)(dirpath)
 
+  const relpath = path.relative(projectRoot, dirpath)
+
+  const mat = await (git as any).statusMatrix({
+    dir: projectRoot,
+    pattern: relpath.length > 0 ? path.join(relpath, "*") : "*"
+  })
+
+  const indexedFiles = mat.map((x: [string, number, number, number]) =>
+    path.join(dirpath, x[0])
+  )
+
   const ret: any = await Promise.all(
     filenames.map(async name => {
       const childPath = path.join(dirpath, name)
       const stat = await pify(fs.stat)(childPath)
+
       return {
         name,
         type: stat.isDirectory() ? "dir" : "file",
-        ignored: await isIgnored(projectRoot, childPath)
+        ignored: !indexedFiles.includes(childPath)
       }
     })
   )
