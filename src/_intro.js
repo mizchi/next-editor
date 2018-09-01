efcfa1ff3b16790e3d5dbece11c2f57945d5ec4f
mizchi <miz404@gmail.com>
2018-09-01 19:15:52 +0900

    refacto: sw loading

--- src/_intro.js
+++ src/_intro.js
@@ -5,33 +5,23 @@ const contentEl = document.querySelector(".content-skeleton")
 let isFirstInstall = navigator.serviceWorker.controller == null
 
 let modal = null
-function showUpgradeModal() {
+function showReloadModal() {
   if (modal == null) {
     modal = document.createElement("div")
     document.body.appendChild(modal)
+    modal.style.position = "absolute"
+    modal.style.outline = "1px solid black"
+    modal.style.right = "10px"
+    modal.style.bottom = "10px"
+    modal.style.padding = "10px"
+    modal.style.backgroundColor = "#fff"
   }
 
   modal.innerHTML = `
-    <div style='position: absolute; outline: 1px solid black; right: 10px; bottom: 10px; width: 350px; height: 50px; background: white; padding: 10px; font-family: Georgia;'>
-      <div>New version available!</div>
-      <span>It will be applied from the next</span> - <button onclick="location.reload()">Reload</button>
+    <div style='width: 230px; height: 20px; font-family: Georgia;'>
+      New version available! <button onclick="location.reload()">Reload</button>
     </div>
   `
-}
-
-async function setupServiceWorker() {
-  navigator.serviceWorker.addEventListener("controllerchange", () => {
-    if (isFirstInstall) {
-      isFirstInstall = false
-    } else {
-      showUpgradeModal()
-    }
-  })
-
-  const reg = await navigator.serviceWorker.register("/sw.js")
-  await navigator.serviceWorker.ready
-  // start sw check
-  setInterval(() => reg.update(), IS_LOCALHOST ? 3 * 1000 : 5 * 60 * 1000)
 }
 
 ;(async () => {
@@ -40,7 +30,21 @@ async function setupServiceWorker() {
     if (USE_SW) {
       contentEl.innerHTML = "Checking service-worker..."
       console.time("loading:sw")
-      await setupServiceWorker()
+
+      // start sw handling
+      navigator.serviceWorker.addEventListener("controllerchange", () => {
+        if (isFirstInstall) {
+          isFirstInstall = false
+        } else {
+          showReloadModal()
+        }
+      })
+
+      const reg = await navigator.serviceWorker.register("/sw.js")
+      await navigator.serviceWorker.ready
+      // start sw check
+      setInterval(() => reg.update(), IS_LOCALHOST ? 3 * 1000 : 5 * 60 * 1000)
+
       console.timeEnd("loading:sw")
       contentEl.innerHTML = ""
     }
